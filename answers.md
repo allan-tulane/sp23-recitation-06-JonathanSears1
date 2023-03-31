82e731af7b55b9e7885bcf669b674c3f15fd06df
Jonathan Sears <112529644+JonathanSears1@users.noreply.github.com>
2023-03-31 01:32:52 +0000

    Finished

--- answers.md
+++ answers.md
@@ -12,15 +12,17 @@ Place all written answers from `recitation-06.md` here for easier grading.
 
 File | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length
 ----------------------------------------------------------------------
-f1.txt    |                     |                |
-alice29.txt    |                     |                |
-asyoulik.txt    |                     |                |
-grammar.lsp    |                     |                |
-fields.c    |                     |                |
+f1.txt        | 2948      |   826    | ~3.5
+alice29.txt   | 2969620   |  676374  | ~4.39
+asyoulik.txt  | 2378401   |  606448  | ~3.92
+grammar.lsp   | 52094     |  17356   | ~3
+fields.c      | 178400    |  56206   | ~3.17
+
+The huffman coding encodings are all around 3-4 times shorter then the fixed length
 
 
 
 
 - **e.**
-
+number of characters * log2(number of unique characters)
 
--- main.py
+++ main.py
@@ -29,33 +29,53 @@ def make_huffman_tree(f):
     # the leaves of the final tree
     for c in f.keys():
         p.put(TreeNode(None,None,(f[c], c)))
-
     # greedily remove the two nodes x and y with lowest frequency,
     # create a new node z with x and y as children,
     # insert z into the priority queue (using an empty character "")
     while (p.qsize() > 1):
-        # TODO
-        
+			# TODO
+      x = p.get()
+      y = p.get()
+      z = TreeNode(x,y,(x.data[0]+y.data[0],""))
+      p.put(z)
     # return root of the tree
     return p.get()
 
 # perform a traversal on the prefix code tree to collect all encodings
 def get_code(node, prefix="", code={}):
     # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+    if node.left == None and node.right == None:
+      code[node.data[1]] = prefix
+    elif node.right == None:
+      get_code(node.left, prefix + "0",code)
+    elif node.left == None:
+      get_code(node.right, prefix + "1", code)
+    else:
+      get_code(node.right, prefix + "1",code)
+      get_code(node.left,prefix + "0", code)
+    return code
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
+  cost = 0
+  numvals = 0
+  for val in f.values():
+    numvals += val
+  cost = len(bin(numvals)) * numvals
+  return cost
 
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
     # TODO
-    pass
+  cost = 0
+  for item in f.items():
+    cost += item[1] * len(C.get(item[0]))
+  return cost
 
-f = get_frequencies('f1.txt')
+f = get_frequencies('fields.c')
 print("Fixed-length cost:  %d" % fixed_length_cost(f))
+
 T = make_huffman_tree(f)
 C = get_code(T)
+print(C)
 print("Huffman cost:  %d" % huffman_cost(C, f))
